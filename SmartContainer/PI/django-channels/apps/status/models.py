fd9c868f8f7548f8f4961ee7c921fc26d64b1e1b
migusdn <migusdn@naver.com>
2019-08-06 02:14:34 +0900

    파이 서버 rest_framework 추가

--- SmartContainer/PI/django-channels/apps/status/models.py
+++ SmartContainer/PI/django-channels/apps/status/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Device(models.Model):
-    Device_num = models.IntegerField
+    Device_num = models.IntegerField()
+    Device_name = models.CharField(max_length=10)
+    Device_status = models.BooleanField()
+
+    def __str__(self):
+        return self.Device_num
