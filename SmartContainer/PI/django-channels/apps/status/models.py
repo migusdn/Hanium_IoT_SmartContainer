491fce55b10605ac09f924b9dce10d60cea46352
박현우 <migusdn@gmail.com>
2019-08-29 18:17:28 +0900

    DB Table 수정

--- SmartContainer/PI/django-channels/apps/status/models.py
+++ SmartContainer/PI/django-channels/apps/status/models.py
@@ -22,10 +22,10 @@ class Device(models.Model):
     UpHumid = models.CharField(max_length=100, null=True)
     DoHumid = models.CharField(max_length=100, null=True)
     # 강제성 유무
-    EnforceT_Up = models.NullBooleanField()
-    EnforceT_Do = models.NullBooleanField()
-    EnforceH_Up = models.NullBooleanField()
-    EnforceH_Do = models.NullBooleanField()
+    EnforceT_Up = models.BooleanField(default=False)
+    EnforceT_Do = models.BooleanField(default=False)
+    EnforceH_Up = models.BooleanField(default=False)
+    EnforceH_Do = models.BooleanField(default=False)
 
     def __str__(self):
         return self.ConId
